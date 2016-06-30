2d00e83396a0793b77558ecb0ceac01ffb5b3426
Chris Caragianis <chriscaragianis@gmail.com>
2016-06-30 12:28:06 -0400

    Simple cases of properties

--- src/SimpleSchema.js
+++ src/SimpleSchema.js
@@ -1,20 +1,44 @@
 import _ from 'lodash';
+import YAML from 'js-yaml';
 
 export default class {
   constructor(schema) {
     this.schema = schema;
   }
-  getProp(key) {
-    return key;
+
+  getType(typeFun) {
+    return `${_.toLower(typeFun.name)}`
   }
+
+  getPropField(key, field) {
+    if (field == 'type') {
+      return `${field}: ${this.getType(this.schema[key][field])}`;
+    }
+    return `${field}: ${this.schema[key][field]}`;
+  }
+
+  // schema, string => template literal
+  getProp(key) {
+    let out = ``;
+    out += `${this.getPropField(key, 'type')}
+        `;
+    out += this.getPropField(key, 'label');
+    return _.trim(out);
+  }
+
+  // schema => template literal
   getProps() {
     let out = ``
     _.keys(this.schema).forEach((key) => {
-      out += `${this.getProp(key)}
+      out += `${key}:
+      `;
+      out += `  ${this.getProp(key)}
       `;
     });
     return _.trim(out);
   }
+
+  // schema => template literal
   toYaml() {
     return `
     type: object
