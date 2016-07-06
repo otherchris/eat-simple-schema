71e45367dd63470538abccf9c20066fa69d0a1a0
Chris Caragianis <chriscaragianis@gmail.com>
2016-07-06 10:47:44 -0400

    Copy all schema files to a given directory (preserve relative path

--- getSchema.rb
+++ getSchema.rb
@@ -1,10 +1,16 @@
 require 'find'
+require 'fileutils'
 
 root_path = ARGV[0];
+dest_path = ARGV[1];
+if !File.directory?(dest_path) then FileUtils.mkdir(dest_path) end
 
-schema_paths = []
 Find.find(root_path) do |path|
-  schema_paths << path if path =~ /.*chema\.js/
+  if path =~ /.*chema\.js/ then
+    path_split = path.match(/(.*\/)(?=[^\/]*$)([^\/]*$)/)
+    dir = "#{dest_path}/#{path_split[1].match(/[^\.\/].*/)[0]}"
+    fname = path_split[2]
+    if !File.directory?(dir) then FileUtils.mkdir_p(dir) end
+    FileUtils.cp_r(path, "#{dir}/#{fname}")
+  end
 end
-
-puts schema_paths
