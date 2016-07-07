475249508584ef4319ebbca09eb1403b35abd2ff
Chris Caragianis <chriscaragianis@gmail.com>
2016-07-07 10:49:10 -0400

    Fix schema imports (YAML comes through)

--- getSchema.rb
+++ getSchema.rb
@@ -2,7 +2,7 @@ require 'find'
 require 'fileutils'
 
 
-import_file = "import _ from 'lodash';\n"
+import_file = ""
 count = 0
 root_path = ARGV[0];
 dest_path = ARGV[1];
@@ -12,15 +12,8 @@ Find.find(root_path) do |path|
   if path =~ /.*chema\.js/ then
     path_end = path.match(/[^\/]*\/[^\/]*$/)[0].sub(/\//, '.')
     FileUtils.cp(path, dest_path + '/' + path_end)
-    import_file << "import * as S#{count} from './#{path_end}';\n"
+    import_file << "export * from './#{path_end}';\n"
     count += 1
   end
 end
-import_file << "const objs = ["
-count.times do |i|
-  import_file << "...S#{i},"
-end
-import_file.chop!
-import_file << "];\n"
-import_file << "export default _.assign(...objs);"
 puts import_file
